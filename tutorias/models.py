5e69fcbdd50bf3d09f4702e3ad26267acc287900
Fran Morón <fj.moron@gmail.com>
2014-11-08 20:10:36 +0100

    Modelo Grado y su relación con Alumno

--- tutorias/models.py
+++ tutorias/models.py
@@ -1,12 +1,25 @@
 from django.db import models
+#Ejemplos sobre relaciones en django
+#https://docs.djangoproject.com/en/1.4/topics/db/examples/
 
 class Alumno(models.Model):
     nombre = models.CharField(max_length=100, null=False)
     apellidos = models.CharField(max_length=200, null=False)
-    dni = models.CharField(max_length=9, null=False)
+    dni = models.CharField(max_length=9, unique=True, null=False)
     usuario = models.CharField(max_length=50, null=False)
     password = models.CharField(max_length=20, null=False)
     email = models.CharField(max_length=200, null=False)
 
-    def __str__(self):
-        return self.nombre + " " + self.apellidos
+    def __unicode__(self):
+    	return self.nombre + " " + self.apellidos
+    class Meta:
+    	ordering = ('apellidos',)
+    		
+class Grado(models.Model):
+	nombre = models.CharField(max_length=200, null=False)
+	alumnos = models.ManyToManyField(Alumno)
+
+	def __unicode__(self):
+		return self.nombre
+	class Meta:
+		ordering = ('nombre',)
